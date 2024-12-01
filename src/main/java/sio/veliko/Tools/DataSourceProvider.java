9781485cd92200a6b88b2c68f41dd2b1d8a345b2
shirelmarciano05 <shirel.marciano05@gmail.com>
2024-12-01 22:42:37 +0100

    marqueur station : ok popup : ok infos sur stations : ok

---
+++ src/main/java/sio/veliko/Tools/DataSourceProvider.java
@@ -0,0 +1,21 @@
+package sio.veliko.Tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
+public class DataSourceProvider {
+
+    private static Connection cnx;
+
+    public DataSourceProvider() throws ClassNotFoundException, SQLException {
+        String pilote = "com.mysql.cj.jdbc.Driver";
+        Class.forName(pilote);
+        cnx = DriverManager.getConnection("jdbc:mysql://localhost/app_db?serverTimezone="
+                + TimeZone.getDefault().getID(), "root", "");
+    }
+    public static Connection getCnx() {
+        return cnx;
+    }
+}
